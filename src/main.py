2ea1fc5094307cc14145164f564c2bcb53f82523
rlegnain <rlegnain@ymail.com>
2015-10-30 00:27:45 -0400

    Add axis labels to the plots

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import numpy as np
 import pyqtgraph as pg
 import PySide 
 
-CHUNK = 1024    #
+CHUNK = 1024    #  CHUNK is power of 2
 samlingRate = 44100 # sampling/second
 CHANNELS = 1
 FORMAT = pyaudio.paInt16
-#freqRange = np.arange(-CHUNK/2 , CHUNK/2)/float(CHUNK)*float(samlingRate)  # two-side fourier transform
-freqRange = np.arange(0 , CHUNK)/float(CHUNK)*float(samlingRate)
+
+OneSideFFT_points = CHUNK/2 + 1      #Calculate the of one-side FFF points.
 window = np.ones(CHUNK)
 
 #class SpectrumAnalyzer(pg.GraphicsWindow):
@@ -28,11 +28,17 @@ class SpectrumAnalyzer(PySide.QtGui.QWidget):
         self.ScreenTIME.setRange(yRange=[-4000,4000])
         self.ScreenTIME.showGrid(x = True, y = True, alpha = 0.3) 
         self.timePlot  = self.ScreenTIME.plot(pen='y', )
+        self.ScreenTIME.setLabels(left=('Amplitue')) 
+        self.ScreenTIME.setLabels(bottom=('Time (ms)')) 
+
+
         ''' Create screen to plot Frequency domain  '''
         self.ScreenFFT = pg.PlotWidget( background=(0, 0, 0))  # define plot windows
-        self.ScreenFFT.setRange(yRange=[5,300000])
+        self.ScreenFFT.setRange(yRange=[5,1000])
         self.ScreenFFT.showGrid(x = True, y = True, alpha = 0.3) 
         self.fftPlot  = self.ScreenFFT.plot(pen='y', )
+        self.ScreenFFT.setLabels(left=('Magnitude')) 
+        self.ScreenFFT.setLabels(bottom=('Frequency (Hz)')) 
 
         ''' Create power ON/OFF button '''
         self.BtnPower = PySide.QtGui.QPushButton("OFF")
@@ -44,6 +50,12 @@ class SpectrumAnalyzer(PySide.QtGui.QWidget):
         self.comBoxWinFunction.addItem("Hanning ")
         self.comBoxWinFunction.addItem("Bartlett ")
         self.comBoxWinFunction.addItem("Blackman ")
+
+        ''' Create list box of channes menu'''
+        self.comBoxChannelList = PySide.QtGui.QComboBox()
+        self.comBoxChannelList.addItem("Channel 1")
+        self.comBoxChannelList.addItem("Channel 2")
+        self.comBoxChannelList.addItem("Both channels")
 
 
         ''' Layouts '''
@@ -57,14 +69,16 @@ class SpectrumAnalyzer(PySide.QtGui.QWidget):
         self.ParameterLyout = PySide.QtGui.QVBoxLayout()
         self.ParameterLyout.addWidget(self.BtnPower)
         self.ParameterLyout.addWidget(self.comBoxWinFunction)
+        self.ParameterLyout.addWidget(self.comBoxChannelList)
+        
+        
+
 
         ''' All Layout'''
         self.Grid = PySide.QtGui.QGridLayout() 
         self.Grid.setSpacing(10)
         self.Grid.addLayout(self.screenLyout, 0,0,16,24)
         self.Grid.addLayout(self.ParameterLyout, 0,25)
-
-
 
         self.setLayout(self.Grid)
         self.show()
@@ -73,7 +87,16 @@ class SpectrumAnalyzer(PySide.QtGui.QWidget):
         #QtCore.QObject.connect(button, QtCore.SIGNAL ('clicked()'), someFunc)
         self.BtnPower.clicked.connect(self.BtnPower_clicked)
         self.comBoxWinFunction.currentIndexChanged[int].connect(self.SelectWindowsFun)
-
+        self.comBoxChannelList.currentIndexChanged[int].connect(self.selectChannel)
+   
+    def selectChannel(self, index):
+        global window
+        if   index == 0:
+            CHANNELS = 1
+        elif index == 2:
+            CHANNELS = 1
+        elif index == 3:
+            CHANNELS = 1
 
     def SelectWindowsFun(self, index):
         global window
@@ -96,7 +119,6 @@ class SpectrumAnalyzer(PySide.QtGui.QWidget):
             self.ON_OFF = False
             self.BtnPower.setText("OFF")
             #self.BtnPower.setStyleSheet('QPushButton {color: red}')
-
         else:
             self.TurnON()
             self.ON_OFF = True
@@ -108,7 +130,7 @@ class SpectrumAnalyzer(PySide.QtGui.QWidget):
         self.openPort()
         self.t = PySide.QtCore.QTimer()
         self.t.timeout.connect(self.update)
-        self.t.start(50) #QTimer takes ms
+        self.t.start(50) # QTimer takes ms
 
 
 
@@ -131,16 +153,24 @@ class SpectrumAnalyzer(PySide.QtGui.QWidget):
         self.p.terminate()
 
     def update(self):        
-        self.SignalProcessing()
+        self.plotOnScreen()
 
-    def SignalProcessing(self):        
+    def calculateFFT(self):
         global window
         timeSignal = self.readSignal()*window
-        self.timePlot.setData(freqRange, timeSignal)
-        #freqSignal = np.fft.fftshift(np.abs(np.fft.fft(timeSignal)))  # plot two side fourier transform
-        freqSignal = np.abs(np.fft.fft(timeSignal))  # plot one-side fourier transform
-        freqSignal_dB = 10*np.log10(freqSignal)
-        self.fftPlot.setData(freqRange[0:CHUNK/2-1], freqSignal[0:CHUNK/2-1])
+        TwoSideFFT = np.fft.fft(timeSignal)/ (CHUNK)   # Two-side FFT.   We devided by CHUNK (please revise FFT)
+        OneSideFFT = 2 * TwoSideFFT[0 : OneSideFFT_points]  # we multiply by 2, because we remove the second FFT side (read FFT)
+        MagnitudeFFT = np.abs(OneSideFFT)
+        #freqSignal_dB = 10*np.log10(freqSignal)
+        timeRange = np.arange(0 , CHUNK) / float(samlingRate)
+        freqRange = np.arange(0 , OneSideFFT_points) * (float(samlingRate)/ float(CHUNK))
+
+        return timeSignal, MagnitudeFFT, timeRange, freqRange
+
+    def plotOnScreen(self):
+        timeSignal, MagnitudeFFT, timeRange, freqRange  = self.calculateFFT()
+        self.timePlot.setData(timeRange, timeSignal)
+        self.fftPlot.setData(freqRange, MagnitudeFFT)
 
 
 if __name__ == '__main__':
