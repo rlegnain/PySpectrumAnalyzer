9a2aad0ca0d9a1131aa7ed7fbfc83bff62ae8002
Legnain <rlegnain@ymail.com>
2015-11-12 16:37:53 -0500

    Add signal slot for setting tab

--- src/Interface/settingUI.py
+++ src/Interface/settingUI.py
@@ -9,7 +9,7 @@ class settingUI(QtGui.QWidget):
 		self.LabelInterface = QtGui.QLabel("Interface")         # Lablel of Interface
 		self.comBoxInterface = QtGui.QComboBox()            # list of interface
 		self.comBoxInterface.addItem("Sound Card")
-		self.comBoxInterface.addItem("USB")
+		#self.comBoxInterface.addItem("USB")
 		
 		#Create Label and Spin Box for sampling Rate 
 		self.LabelSampleRate = QtGui.QLabel("Sampling Rate")         
@@ -17,13 +17,14 @@ class settingUI(QtGui.QWidget):
 		self.comBoxSamplingRate.addItem("44100")
 		self.comBoxSamplingRate.addItem("88200")
 		self.comBoxSamplingRate.addItem("96000")
-		
-		
+		self.comBoxSamplingRate.currentIndexChanged[int].connect(self.SelectSamplingRate)
+
 		# Create label and Spin  Box for Chunck
 		self.LabelChunck = QtGui.QLabel("Chunk")         # Lablel of Interface
 		self.comBoxCunck= QtGui.QComboBox()
 		self.comBoxCunck.addItem("1024")
 		self.comBoxCunck.addItem("2048")
+		self.comBoxSamplingRate.currentIndexChanged[int].connect(self.SelectChunck)
 		
 		# Creat Label and Spin Box for Format
 		self.LabelFormat = QtGui.QLabel("Format")
@@ -53,5 +54,21 @@ class settingUI(QtGui.QWidget):
 		self.HLayout.addLayout(self.VLayout)
 		self.HLayout.addStretch(1)
 
-		
 		self.setLayout(self.HLayout)
+		
+		# function
+	def SelectSamplingRate (self, index):
+		if index=="44100":
+			samplingRate = 44100
+		elif index=="88200":
+			samplingRate = 88200
+		elif index=="96000":
+			samplingRate = 96000
+			
+				
+	def SelectChunck(self, index):
+		if index=="1024":
+			samplingRate = 1024
+		elif index=="2048":
+			samplingRate = 2048
+				
